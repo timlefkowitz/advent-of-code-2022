82dd89bcf55f5971b40a9857d111693d918b3780
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2023-01-12 15:03:31 -0600

    Update day9.java

--- src/main/java/day9.java
+++ src/main/java/day9.java
@@ -6,24 +6,43 @@ import java.util.List;
 
 public class day9 {
 
-    public static final int gridSize =20;
+    public static final int gridSize =1000;
 
-    public static int posHEADX =10;
-    public static int posHEADY = 10;
-    public static int posTAILX =10;
-    public static int posTAILY = 10;
+    public static int posHEADX =500;
+    public static int posHEADY =500;
+    public static int posTAILX =500;
+    public static int posTAILY =500;
     public static int[] grid = new int[gridSize*gridSize];
     public static void main(String[] args) throws IOException {
 
 
         BufferedReader br = new BufferedReader(new FileReader("inputs/day9.txt"));
-        String line;
+//        String line;
+        String input =" R 4\n" +
+                "U 4\n" +
+                "L 3\n" +
+                "D 1\n" +
+                "R 4\n" +
+                "D 1\n" +
+                "L 5\n" +
+                "R 2";
+        String line="";
         List<String> rows = new ArrayList<>();
         while ((line = br.readLine()) != null) {
-            System.out.println(line);
-            String[] move = line.split("\n");
+//        for(String line : input.split("\n")){
+//            System.out.println(line);
+            String[] move = line.split(" ");
             move(move[0], Integer.parseInt(move[1]));
         }
+//        int amount = count();
+
+        int amount =0;
+        for (int i: grid){
+            if(i>0){
+                amount++;
+            }
+        }
+        System.out.println(amount);
     }
     private static void move(String dir, int amount){
         for(int i=0;i<amount;i++){
@@ -39,40 +58,44 @@ public class day9 {
             case "D":
                 posHEADY += 1;
                 break;
-            case "R":
-                posHEADY -= 1;
-                break;
             case "L":
-                posHEADY += 1;
+                posHEADX -= 1;
+                break;
+            case "R":
+                posHEADX += 1;
                 break;
         }
 
         if (Math.abs(posHEADX - posTAILX) > 1){
-            posTAILX += (posHEADX-posTAILX)-1;
+            if("L".equals(dir)){
+                posTAILX -= 1;
+            } else {
+                posTAILX += 1;
+            }
+
             posTAILY = posHEADY;
-            System.out.println();
+//            System.out.println();
         }
         if (Math.abs(posHEADY - posTAILY) > 1){
-            posTAILY += (posHEADY-posTAILY)-1;
+            if("U".equals(dir)){
+                posTAILY -= 1;
+            } else {
+                posTAILY += 1;
+            }
             posTAILX = posHEADX;
-            System.out.println();
+//            System.out.println();
         }
 
         grid[posTAILY * gridSize + posTAILX ] +=1;
-        printGrid();
+//        printGrid();
 
         System.out.println("");
-
-
-
-
-
     }
 
     private static void printGrid() {
         for(int y =0;y<gridSize;y++){
             for(int xs=0;xs< gridSize;xs++){
-                System.out.println(grid[y*gridSize+xs]);
+                System.out.print(grid[y * gridSize + xs]);
             }
             System.out.println();
         }
